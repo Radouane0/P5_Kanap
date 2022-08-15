7673a34b8f7586d200e240d4df6e4d2e02645e39
Radouane <ikramaboufera@gmail.com>
2022-08-15 16:19:00 +0200

    Finalisation de l'étape 3 et 4

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,40 @@
-console.log("index")
+fetch("http://localhost:3000/api/products")
+    .then(res => res.json())
+    .then(data => ShowProducts(data))
+
+function ShowProducts(data) {
+    for (Product of data) {
+        console.log(Product)
+        const SectionProducts = document.getElementById("items")
+        const BaliseA = document.createElement("a")
+        BaliseA.href = `./product.html?id=${Product._id}`
+        SectionProducts.appendChild(BaliseA)
+
+        const Article = document.createElement("article")
+        BaliseA.appendChild(Article)
+
+        const Image = document.createElement("img")
+        Image.src = Product.imageUrl
+        Image.alt = Product.altTxt
+        Article.appendChild(Image)
+
+        const NomProduit = document.createElement("h3")
+        NomProduit.className = "productName"
+        NomProduit.innerText = Product.name
+        Article.appendChild(NomProduit)
+
+        const Description = document.createElement("p")
+        Description.className = "productDescription"
+        Description.innerText = Product.description
+        Article.appendChild(Description)
+
+    }
+    
+}
+
+
+
+
+
+
+
