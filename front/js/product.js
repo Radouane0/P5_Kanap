adff04749a3ba70da33dcfd3dea60690cb037a36
Radouane <ikramaboufera@gmail.com>
2022-08-18 16:53:47 +0200

    Finalisation étape 5 et début étape 6

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,15 @@
-console.log("product")
+const url =  window.location.search;
+const urlSearchParams = new URLSearchParams(url)
+const id = urlSearchParams.get("id")
+console.log(id)
+
+function ShowProduct(data) {
+    for (Product of data) {
+        console.log(Product)
+        const ImageProduct = document.getElementsByClassName("item__img") 
+        const Img = document.createElement("img")
+        Img.src = Product.imageUrl
+        Img.alt = Product.altTxt
+        ImageProduct.appendChild(Img)
+    }
+}
